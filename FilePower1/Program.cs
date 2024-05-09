0b580d92a9fa5050bce363822bdd7aea2de7135e
Daniela Sousa <danielasousa.info@gmail.com>
2024-05-09 11:30:46 +0100

    Correção de linhas de código

--- FilePower1/Program.cs
+++ FilePower1/Program.cs
@@ -1,6 +1,6 @@
 ﻿using System;
-using System.Text;
 using System.IO;
+using System.Text;
 using System.Collections.Generic;
 
 namespace FilePower1
@@ -9,33 +9,30 @@ namespace FilePower1
     {
         static void Main(string[] args)
         {
+            if (args.Length < 1)
+            {
+                Console.WriteLine("Please provide a file name.");
+                return;
+            }
 
-        if (args.Length < 1)
-        {
-            Console.WriteLine("Please provide a file name.");
-            return;
-        }
+            string fileName = args[0];
+            Queue<string> queue = new Queue<string>();
 
-        string fileName = args[0];
-        Queue<string> queue = new Queue<string>();
+            Console.WriteLine("Insert a name to the file. Press ENTER to stop.");
+            string input;
+            while ((input = Console.ReadLine())!= "")
+            {
+                queue.Enqueue(input);
+            }
 
-        Console.WriteLine("Insert a name to the file. Press ENTER to stop.");
-        string input;
-
-        while ((input = Console.ReadLine())!= "")
-        {
-            queue.Enqueue(input);
-        }
-        using (StreamWriter writer = new StreamWriter(fileName))
-        {
+            StreamWriter writer = new StreamWriter(fileName);
             while (queue.Count > 0)
             {
                 string line = queue.Dequeue();
                 writer.WriteLine(line);
             }
-        }
-        Console.WriteLine("Saved successfully!.");
-        }
-        
+            writer.Close();
+            Console.WriteLine("Saved successfully!.");
+        }  
     }
 }
