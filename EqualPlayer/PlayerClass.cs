6d537e44e932d6cdb5f77a38a4becba7de3d5c9a
Daniela Sousa <danielasousa.info@gmail.com>
2024-05-09 10:05:24 +0100

    Criado um enum com lista de nomes

--- EqualPlayer/PlayerClass.cs
+++ EqualPlayer/PlayerClass.cs
@@ -1,12 +1,11 @@
 using System;
 using System.Collections.Generic;
-using System.Linq;
-using System.Threading.Tasks;
 
 namespace EqualPlayer
 {
-    public class PlayerClass
-    {
-        
+    public enum PlayerClass
+    {  
+        Tank, Fighter, Slayer,
+        Mage, Controller, Marksmen
     }
 }
