59dffb33da11727fd2a8d2fef3f589b9162aafc9
Daniela Sousa <danielasousa.info@gmail.com>
2024-05-09 10:51:34 +0100

    Ex.5 testado e concluido

--- FilePower1/Program.cs
+++ FilePower1/Program.cs
@@ -1,4 +1,7 @@
 ﻿using System;
+using System.Text;
+using System.IO;
+using System.Collections.Generic;
 
 namespace FilePower1
 {
@@ -6,7 +9,33 @@ namespace FilePower1
     {
         static void Main(string[] args)
         {
-            Console.WriteLine("Hello, World!");
+
+        if (args.Length < 1)
+        {
+            Console.WriteLine("Please provide a file name.");
+            return;
         }
+
+        string fileName = args[0];
+        Queue<string> queue = new Queue<string>();
+
+        Console.WriteLine("Insert a name to the file. Press ENTER to stop.");
+        string input;
+
+        while ((input = Console.ReadLine())!= "")
+        {
+            queue.Enqueue(input);
+        }
+        using (StreamWriter writer = new StreamWriter(fileName))
+        {
+            while (queue.Count > 0)
+            {
+                string line = queue.Dequeue();
+                writer.WriteLine(line);
+            }
+        }
+        Console.WriteLine("Saved successfully!.");
+        }
+        
     }
 }
