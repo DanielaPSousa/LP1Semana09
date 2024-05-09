c0a2e8665b00d25c3f8256f753a0393b0dfb811f
Daniela Sousa <danielasousa.info@gmail.com>
2024-05-09 10:08:56 +0100

    Criado propriedades e construtor

--- EqualPlayer/Player.cs
+++ EqualPlayer/Player.cs
@@ -1,12 +1,17 @@
 using System;
 using System.Collections.Generic;
-using System.Linq;
-using System.Threading.Tasks;
 
 namespace EqualPlayer
 {
     public class Player
     {
-        
+        public PlayerClass PC { get; }
+        public string Name { get; }
+
+        public Player(PlayerClass pClass, string name)
+        {
+            PC = pClass;
+            Name = name;
+        }
     }
 }
