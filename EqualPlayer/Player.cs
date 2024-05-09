ceeb711d2bb5d3998f3541a936abe2fc4b9d2852
Daniela Sousa <danielasousa.info@gmail.com>
2024-05-09 10:10:57 +0100

    Criado métodos Equals e GetHashCode

--- EqualPlayer/Player.cs
+++ EqualPlayer/Player.cs
@@ -13,5 +13,20 @@ namespace EqualPlayer
             PC = pClass;
             Name = name;
         }
+        public override bool Equals(object obj)
+        {
+            if (obj == null || GetType()!= obj.GetType())
+            {
+                return false;
+            }
+
+            Player other = (Player)obj;
+            return PC == other.PC && Name == other.Name;
+        }
+
+        public override int GetHashCode()
+        {
+            return PC.GetHashCode() ^ Name.GetHashCode();
+        }
     }
 }
